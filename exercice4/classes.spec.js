684f6c59a61c5bcd9a09ccd69d9560c699498072
pierre_cours <pierre_cours@MacBook-Pro-80.local>
2024-05-16 14:15:26 +0200

    oui

--- exercice4/classes.spec.js
+++ exercice4/classes.spec.js
@@ -1,8 +1,13 @@
+const User = require('./classes')
+
 describe("The isValid function", () => {
     it("should call validateName, validateEmail, validatePassword functions when isValid fn is called", () => {
       // arrange
       const user = new User();
   
+      jest.spyOn(user, 'validateName');
+      jest.spyOn(user, 'validateEmail');
+      jest.spyOn(user, 'validatePassword');
   
       // action
       user.isValid();
